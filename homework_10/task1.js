11995e0894edbabb4c9b3c3e671f19a35905e1bb
Tetiana Okhotiuk <tkhedyk@gmail.com>
2018-02-24 22:15:11 +0200

    uploading hw#10

---
+++ homework_10/task1.js
@@ -0,0 +1,33 @@
+ function debounce(func, delay) {
+  var timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(func, delay);
+  };
+}
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+let increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 1'
+
