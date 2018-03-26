73010f4bf9690f5d9a95a909e94029da29d04816
Tetiana Okhotiuk <tkhedyk@gmail.com>
2018-03-26 16:58:13 +0300

    corrections

--- homework_03/js/main.js
+++ homework_03/js/main.js
@@ -1,69 +1,70 @@
-var sumSalary = 0;
-var sumAge = 0;
-var id = 0;
 
 function Company(obj) {
-    var listEmployees = [];
-    var reservedList = [];
+    let sumSalary = 0;
+    let sumAge = 0;
+    let id = 0;
+    let listEmployees = [];
+    let reservedList = [];
+    let timeStart = Date();
+    let timeBegin = Date.now() / 1000;
+    let finishTime = Date();
+    let timeEnd = Date.now() / 1000;
     this.name = obj.name;
     this.owner = obj.owner;
     this.maxCount = obj.maxCompanySize;
-    this.addNewEmployee = function(employee) {
-        if (typeof(employee) === 'number') {
-            console.log(`Please try to add Employee instance`);
-        }
-
-        if (this.maxCount > listEmployees.length) {
-
+    this.addNewCompany = function(employee) {
             employee.company = {};
             employee.company.name = this.name;
             employee.hire(employee, this.name);
-            employee.company.timeStart = Date();
-            employee.company.timeBegin = Date.now() / 1000;
+            employee.company.timeStart = timeStart;
+            employee.company.timeBegin = timeBegin;
+            employee.currentTimeStart = timeStart;
             employee.id = id;
             ++id;
-
-
             listEmployees.push(employee);
             reservedList.push(employee);
+    }
+
+    this.setFinishEndTime = function(value) {
+      value.company.finishTime = finishTime;
+      value.company.timeEnd = timeEnd;
+      value.fire();
+    }
+
+    this.addNewEmployee = function(employee) {
+        if (employee instanceof Employee) {
+
+        if (this.maxCount > listEmployees.length) {
+
+            this.addNewCompany(employee);
 
         } else {
-            var minSalary = listEmployees[0].salary;
-            for (var i = 0; i < listEmployees.length; i++) {
+            let minSalary = listEmployees[0].salary;
+            for (let i = 0; i < listEmployees.length; i++) {
                 if (listEmployees[i].salary < minSalary) {
                     minSalary = listEmployees[i].salary;
-
                 }
             }
 
-            for (var i = 0; i < listEmployees.length; i++) {
+            for (let i = 0; i < listEmployees.length; i++) {
                 if (listEmployees[i].salary === minSalary) {
-                    employee.company = {};
-                    employee.company.name = this.name;
-                    employee.hire(employee, this.name);
-                    employee.company.timeStart = Date();
-                    employee.company.timeBegin = Date.now() / 1000;
-                    employee.id = id;
-                    ++id;
-                    reservedList.push(employee);
-                    reservedList[listEmployees[i].id].company.finishTime = Date();
-                    reservedList[listEmployees[i].id].company.timeEnd = Date.now() / 1000;
-                    reservedList[listEmployees[i].id].fire();
-                    listEmployees.splice(i, 1, employee);
+                    this.addNewCompany(employee);
+                    this.setFinishEndTime(reservedList[listEmployees[i].id]);
+                    listEmployees.splice(i, 1);
                 }
             }
         }
 
+      } else {
+           console.log(`Please try to add Employee instance`);
+      }
     };
 
     this.removeEmployee = function(idi) {
 
-
-        for (var i = 0; i < reservedList.length; i++) {
+        for (let i = 0; i < reservedList.length; i++) {
             if (reservedList[i].id === listEmployees[idi].id) {
-                reservedList[i].company.finishTime = Date();
-                reservedList[i].company.timeEnd = Date.now() / 1000;
-                reservedList[i].fire();
+                this.setFinishEndTime(reservedList[i]);
             }
         }
 
@@ -71,39 +72,39 @@ function Company(obj) {
     }
 
     this.getAvarageSalary = function() {
-        for (var i = 0; i < listEmployees.length; i++) {
+        for (let i = 0; i < listEmployees.length; i++) {
             sumSalary = sumSalary + listEmployees[i].salary;
         }
 
-        var avarageSalary = sumSalary / listEmployees.length;
+        let avarageSalary = sumSalary / listEmployees.length;
         console.log(avarageSalary);
     }
 
     this.getAvarageAge = function() {
-        for (var i = 0; i < listEmployees.length; i++) {
+        for (let i = 0; i < listEmployees.length; i++) {
             sumAge = sumAge + listEmployees[i].age;
         }
 
-        var avarageAge = sumAge / listEmployees.length;
+        let avarageAge = sumAge / listEmployees.length;
         console.log(avarageAge);
     }
 
     this.getEmployees = function() {
-        var _privateList = listEmployees
+        let _privateList = listEmployees;
         console.log(_privateList);
     }
 
     this.getFormattedListOfEmployees = function() {
-        for (var i = 0; i < listEmployees.length; i++) {
+        for (let i = 0; i < listEmployees.length; i++) {
             console.log(`${listEmployees[i].name} - works in ${this.name} ${parseInt(Date.now()/1000 - listEmployees[i].company.timeBegin)} seconds`)
         }
     }
 
     this.getHistory = function() {
-        var _log = reservedList;
-        var creationDate = Date();
+        let _log = reservedList;
+        let creationDate = Date();
         console.log(`${this.name} was created in ${creationDate}`);
-        for (var i = 0; i < _log.length; i++) {
+        for (let i = 0; i < _log.length; i++) {
 
             if (_log[i].company.timeStart) {
                 console.log(`${_log[i].name} starts working at ${this.name} in ${_log[i].company.timeStart}`)
@@ -113,80 +114,70 @@ function Company(obj) {
                 console.log(`${_log[i].name} ends working at ${this.name} in ${_log[i].company.finishTime}`)
             }
         }
-
     }
-
 }
 
 function Employee(obj) {
 
-    var workHistory = [];
+    let workHistory = [];
     this.name = obj.name;
     this.primarySkill = obj.primarySkill;
     this.age = obj.age;
     this.salary = obj.salary;
-
     this.getSalary = function() {
         return this.salary;
     }
 
     this.setSalary = function(value) {
-        var _salary = this.getSalary();
-        if (!isFinite(this.salary)) {
-            return console.log('salary error')
-        }
-
+        let _salary = this.getSalary();  
+        if (Number.isFinite(value) || !isNaN(value)) {
         if (_salary > value) {
-
             console.log(`try to change salary from ${_salary} to ${value}`)
         } else if (_salary < value) {
             console.log(`change salary from ${_salary} to ${value}`)
         }
-
+      } 
     }
 
     this.getWorkTimeInSeconds = function() {
-        var workingTime = 0;
-        for (var i = 0; i < workHistory.length; i++) {
+        let workingTime = 0;
+        for (let i = 0; i < workHistory.length; i++) {
             workingTime = workingTime + workHistory[i].timeEnd - workHistory[i].timeBegin;
         }
-        var currentWorkTime = Date.now() / 1000 - this.company.timeBegin;
-        var totalTime = workingTime + currentWorkTime;
+        let currentWorkTime = Date.now() / 1000 - this.company.timeBegin;
+        let totalTime = workingTime + currentWorkTime;
         console.log(totalTime);
     }
 
     this.hire = function(employee, name) {
         employee.currentCompany = name;
-        employee.currentTimeStart = Date();
     }
 
     this.fire = function() {
         delete this.currentCompany;
-        workHistory.push(this.company);
         delete this.currentTimeStart;
-
+        workHistory.push(this.company);
     }
 
     this.getHistory = function() {
-        var _log = workHistory;
+        let _log = workHistory;
+
+        for (let i = 0; i < _log.length; i++) {
+
+            if (_log[i].timeStart) {
+                console.log(`${this.name} is hired to ${_log[i].name} in ${_log[i].timeStart}`)
+            }
+
+            if (_log[i].finishTime) {
+                console.log(`${this.name} is fired from ${_log[i].name} in ${_log[i].finishTime}`)
+            }
+        }
 
         if (this.currentTimeStart) {
             console.log(`${this.name} is hired to ${this.currentCompany} in ${this.currentTimeStart}`)
         }
 
-
-        for (var i = 0; i < _log.length; i++) {
-            if (_log[i].timeStart) {
-                console.log(`${this.name} is hired to ${this.company.name} in ${this.company.timeStart}`)
-            }
-
-            if (_log[i].finishTime) {
-                console.log(`${this.name} is fired from ${this.company.name} in ${this.company.finishTime}`)
-            }
-        }
-
         this.setSalary();
-
     }
 }
 
@@ -207,15 +198,13 @@ epam.addNewEmployee(anton);
 
 console.log(epam.getHistory());
 
-
 epam.removeEmployee(2);
-
 
 console.log(vasyl.getHistory());
 
 console.log(epam.getAvarageSalary()); 
 console.log(epam.getAvarageAge());  
-// epam.addNewEmployee(5,6,9,5); 
+epam.addNewEmployee(5,6,9,5); 
 
 setTimeout(() => {
    epam.removeEmployee(1);
@@ -225,6 +214,7 @@ setTimeout(() => {
 vova.setSalary(900);
 vova.setSalary(2200);
 console.log(vova.getHistory());
+
 
 
 
