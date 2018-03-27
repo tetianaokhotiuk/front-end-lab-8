70f0d8a5b8b75228a50e2451c78d4ff7393e6075
Tetiana Okhotiuk <tkhedyk@gmail.com>
2018-03-27 14:53:43 +0300

    js code formatting

--- homework_04/scripts/script.js
+++ homework_04/scripts/script.js
@@ -33,43 +33,43 @@ function Fighter(obj) {
 	this._enrageAmount = 0;
 }
 
-Fighter.prototype.getHitpoints = function() {
+Fighter.prototype.getHitpoints = function () {
 	console.log(this._hitpoints);
 }
 
-Fighter.prototype.setHitpoints = function(value) {
-		this._hitpoints = value;
-	}
+Fighter.prototype.setHitpoints = function (value) {
+	this._hitpoints = value;
+}
 
-Fighter.prototype.getTotalHitpoints = function() {
+Fighter.prototype.getTotalHitpoints = function () {
 	console.log(this._totalHitpoints);
 }
 
-Fighter.prototype.setTotalHitpoints = function(value) {
+Fighter.prototype.setTotalHitpoints = function (value) {
 	this._totalHitpoints = value;
 }
 
-Fighter.prototype.getAttack = function() {
+Fighter.prototype.getAttack = function () {
 	this._attack++;
 	console.log(this._attack);
 }
 
-Fighter.prototype.setAttack = function(value) {
+Fighter.prototype.setAttack = function (value) {
 	this._attack = value;
 }
 
-Fighter.prototype.fight = function(fighter) {
-	
-	if (fighter !== this && fighter.isAlive) {
+Fighter.prototype.fight = function (fighter) {
 
-		if(fighter._defence) {
+	if (fighter !== this && fighter.isAlive()) {
+
+		if (fighter._defence) {
 			fighter._totalHitpoints++;
 			fighter._defence = false;
-	
-		} else if  (this._isenrage && this._enrageAmount<2) {
 
-		fighter._hitpoints -=  this._attack*2;
-		this._enrageAmount++;
+		} else if (this._enrage && this._enrageAmount < 2) {
+
+			fighter._hitpoints -= this._attack * 2;
+			this._enrageAmount++;
 
 		} else {
 			fighter._hitpoints -= this._attack;
@@ -77,7 +77,7 @@ Fighter.prototype.fight = function(fighter) {
 
 	} else if (fighter === this) {
 		console.log(`you can't fight against yourself`)
-	} else if (fighter._hitpoints<0) {
+	} else if (!fighter.isAlive()) {
 		console.log(`you can't fight against died body`);
 	}
 
@@ -86,43 +86,43 @@ Fighter.prototype.fight = function(fighter) {
 	}
 }
 
-Fighter.prototype.isAlive = function() {
+Fighter.prototype.isAlive = function () {
 	return this._hitpoints > 0;
 }
 
 function Champion(obj) {
-	Fighter.call(this,obj);
+	Fighter.call(this, obj);
 }
 
 Champion.prototype = Object.create(Fighter.prototype);
 Champion.prototype.constructor = Fighter;
 
-Champion.prototype.heal = function() {
-		if (this._hitpoints<this._totalHitpoints) {
-			this._hitpoints +=5;
-		}
+Champion.prototype.heal = function () {
+	if (this._hitpoints < this._totalHitpoints) {
+		this._hitpoints += 5;
 	}
-
-Champion.prototype.defence = function() {
-	this._defence = true;	
 }
 
-Champion.prototype.training = function() {
+Champion.prototype.defence = function () {
+	this._defence = true;
+}
+
+Champion.prototype.training = function () {
 	return this._attack++;
 }
 
 function Monster(obj) {
-	Fighter.call(this,obj);	
+	Fighter.call(this, obj);
 }
 
 Monster.prototype = Object.create(Fighter.prototype);
 Monster.prototype.constructor = Fighter;
 
-Monster.prototype.enrage = function() {
-			this._isenrage = true;
-		}
-		
-Monster.prototype.fury = function() {
+Monster.prototype.enrage = function () {
+	this._enrage = true;
+}
+
+Monster.prototype.fury = function () {
 	if (this._hitpoints >= 5 && this._totalHitpoints >= 5) {
 		this._hitpoints -= 5;
 		this._totalHitpoints -= 5;
@@ -130,12 +130,9 @@ Monster.prototype.fury = function() {
 	}
 }
 
-Monster.prototype.training = function(obj) {
-	this._hitpoints = Math.floor(this._hitpoints + obj.totalHitpoints*0.25);
-	this._totalHitpoints = Math.floor(this._totalHitpoints + obj.totalHitpoints*0.1);
+Monster.prototype.training = function (obj) {
+	this._hitpoints = Math.floor(this._hitpoints + obj.totalHitpoints * 0.25);
+	this._totalHitpoints = Math.floor(this._totalHitpoints + obj.totalHitpoints * 0.1);
 }
-
-
-
 
 
