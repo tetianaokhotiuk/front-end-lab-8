00e015e3807feddfbc82d0f2997a52b54dd0a606
Tetiana Okhotiuk <tkhedyk@gmail.com>
2018-04-08 23:10:30 +0300

    set right order of folders and final formatting

--- homework_07/js/index.js
+++ homework_07/js/index.js
@@ -5,117 +5,105 @@ let allButs = document.getElementsByClassName('but');
 let allItems = document.getElementsByClassName('item');
 let gamers = document.getElementsByClassName('gamer');
 
+function click() {
+    turn++;
+    if (turn % 2 === 0) {
+        $(this).addClass('bulbWhite');
+        $(".bulbWhite").unbind('click', click);
+        $('#gamer2').addClass('turn');
+        $('#gamer1').removeClass('turn');
 
-$(document).ready(function(){
+    } else {
+        $(this).addClass('bulb');
+        $(".bulb").unbind('click', click);
+        $('#gamer1').addClass('turn');
+        $('#gamer2').removeClass('turn');
+    }
 
-    $(".but").click(function(){
-        
-        turn++;
-        if (turn%2===0) {
-        	$(this).addClass('bulbWhite');
-        	// gamers[1].className = 'turn';
-        	// gamers[0].className = 'gamer';
-        } else {
-        	$(this).addClass('bulb');
-        	// gamers[0].className = 'turn';
-        	// gamers[1].className = 'gamer';
-        }
-        
-		checkWinner('but bulb', 0,'Gamer 1', 'but bulb red');
-		checkWinner('but bulbWhite', 1,'Gamer 2', 'but bulbWhite red');
+    checkWinner('but bulb', 0, 'Gamer 1', 'but bulb finish');
+    checkWinner('but bulbWhite', 1, 'Gamer 2', 'but bulbWhite finish');
+}
 
-    });
+$(document).ready(function() {
+    $(".but").bind('click', click);
+    $("div#unclickable").unbind('click', click);
 });
 
-function addBoard(){
-   for (var i = 0; i < itemLength; i++) {        
-             $("#board").append('<div class="item"></div>');             
+function addBoard() {
+    for (let i = 0; i < itemLength; i++) {
+        $("#board").append('<div class="item"></div>');
     }
 
     $('.item').append('<div class="but"></div>');
 
-    for (var i = 210; i < itemLength; i++) {
-       	allItems[i].innerHTML = '';
-       }
+    for (let i = 210; i < itemLength; i++) {
+        allButs[i].id = 'unclickable';
+    }
 
-   let j = 14;
-
-   while ( j < itemLength) {
-       	allItems[j].innerHTML = '';
-       	j +=15;
-       }
+    let j = 14;
+    while (j < itemLength) {
+        allButs[j].id = 'unclickable';
+        j += 15;
+    }
 }
 
- addBoard();
+addBoard();
 
- function buttonCreation() {
- 	$("#board").after('<button type="submit" class="button">New game</button>');
+function buttonCreation() {
+    $(".final").append('<button type="submit" class="button">New game</button>');    
     $('.button').click(function() {
-	 	location.reload();
-	 })
- }
+        location.reload();
+    });
+}
 
- function checkWinner(style, gamer, winner, newStyle) {
- 	
- 	for (var i = 0; i < boardLength; i++) {
- 		if ((allButs[i].className == style) && (allButs[i+1].className == style) && (allButs[i+2].className == style) && (allButs[i+3].className == style))
- 		
- 		{
- 			allButs[i].className = newStyle;
- 			allButs[i+1].className = newStyle;
- 		    allButs[i+2].className = newStyle;
- 		    allButs[i+3].className = newStyle;
+buttonCreation();
 
- 		    gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
- 		    gamers[gamer].className = 'winnerText';
- 		    buttonCreation();	
-        $( "div" ).click(function( event ) {
-            event.stopImmediatePropagation();
-        });
- 		}
+function checkWinner(style, gamer, winner, newStyle) {
 
- 		if ((allButs[i].className == style) && (allButs[i+14].className == style) && (allButs[i+28].className == style) && (allButs[i+42].className == style)) {
- 			allButs[i].className = newStyle;
- 			allButs[i+14].className = newStyle;
- 		    allButs[i+28].className = newStyle;
- 		    allButs[i+42].className = newStyle;
+    for (let i = 0; i < itemLength; i++) {
+        if ((allButs[i].className == style) && (allButs[i + 1].className == style) && (allButs[i + 2].className == style) && (allButs[i + 3].className == style)) {
+            allButs[i].className = newStyle;
+            allButs[i + 1].className = newStyle;
+            allButs[i + 2].className = newStyle;
+            allButs[i + 3].className = newStyle;
+            gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
+            gamers[gamer].className = 'winnerText';
+            $('.header').css( "padding", "15px" );
+            $(".but").unbind('click', click);
+        }
 
- 		    gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
- 		    gamers[gamer].className = 'winnerText';
- 		    buttonCreation();
- 		}
+        if ((allButs[i].className == style) && (allButs[i + 14].className == style) && (allButs[i + 28].className == style) && (allButs[i + 42].className == style)) {
+            allButs[i].className = newStyle;
+            allButs[i + 14].className = newStyle;
+            allButs[i + 28].className = newStyle;
+            allButs[i + 42].className = newStyle;
+            gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
+            gamers[gamer].className = 'winnerText';
+            $('.header').css( "padding", "15px" );
+            $(".but").unbind('click', click);
+        }
 
- 		if ((allButs[i].className == style) && (allButs[i+15].className == style) && (allButs[i+30].className == style) && (allButs[i+45].className == style)) {
- 			allButs[i].className = newStyle;
- 			allButs[i+15].className = newStyle;
- 		    allButs[i+30].className = newStyle;
- 		    allButs[i+45].className = newStyle;
+        if ((allButs[i].className == style) && (allButs[i + 15].className == style) && (allButs[i + 30].className == style) && (allButs[i + 45].className == style)) {
+            allButs[i].className = newStyle;
+            allButs[i + 15].className = newStyle;
+            allButs[i + 30].className = newStyle;
+            allButs[i + 45].className = newStyle;
+            gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
+            gamers[gamer].className = 'winnerText';
+            $('.header').css( "padding", "15px" );
+            $(".but").unbind('click', click);
+        }
 
- 		    gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
- 		    gamers[gamer].className = 'winnerText';
- 		    buttonCreation();
- 		}
-
- 		if ((allButs[i].className == style) && (allButs[i+13].className == style) && (allButs[i+26].className == style) && (allButs[i+39].className == style)) {
- 			allButs[i].className = newStyle;
- 			allButs[i+13].className = newStyle;
- 		    allButs[i+26].className = newStyle;
- 		    allButs[i+39].className = newStyle;
-
- 		    gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
- 		    gamers[gamer].className = 'winnerText';
- 		    buttonCreation();
- 		}
- 		
-
- 		
- 	}
- } 
-
- 
-
-console.log(allItems);
-console.log(allButs);
-console.log(gamers);
-
+        if ((allButs[i].className == style) && (allButs[i + 16].className == style) && (allButs[i + 32].className == style) && (allButs[i + 48].className == style)) {
+            allButs[i].className = newStyle;
+            allButs[i + 16].className = newStyle;
+            allButs[i + 32].className = newStyle;
+            allButs[i + 48].className = newStyle;
+            gamers[gamer].innerHTML = `${winner} won!!! Congrats!`
+            gamers[gamer].className = 'winnerText';
+            $('.header').css( "padding", "15px" );
+            $(".but").unbind('click', click);
+        }
+    }
+}
 
